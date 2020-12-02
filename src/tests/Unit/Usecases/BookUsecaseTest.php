b08fde7148c98c70858f8a7eae191385f8136f5c
Canon11 <10767355+Canon11@users.noreply.github.com>
2020-12-03 00:21:32 +0900

    implement usecase test

---
+++ src/tests/Unit/Usecases/BookUsecaseTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Unit\Usecases;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Usecases\{BookUsecase, AuthorNotFoundException};
+use App\Repositories\{AuthorRepository,BookRepository};
+use App\Entities\{Book,Author};
+use Hamcrest\Matchers;
+use Mockery;
+
+class BookUsecaseTest extends TestCase
+{
+    public function testCreateBookSucceed()
+    {
+        $givenAuthorId = 10;
+        $givenTitle    = 'first PHP';
+        $wantBook      = new Book(2, $givenAuthorId, $givenTitle);
+
+        // arrange
+        $authorRepository = Mockery::mock(AuthorRepository::class);
+        $authorRepository
+            ->shouldReceive('getOrNullById')
+            ->with($givenAuthorId)
+            ->andReturn(new Author($givenAuthorId, 'murata'));
+
+        $bookRepository = Mockery::mock(BookRepository::class);
+        $bookRepository
+            ->shouldReceive('create')
+            ->with(Matchers::equalTo(Book::new($givenAuthorId, $givenTitle)))
+            ->andReturn($wantBook)
+            ->once();
+
+        // action
+        $usecase = new BookUsecase($authorRepository, $bookRepository);
+        $createdBook = $usecase->createBook($givenAuthorId, $givenTitle);
+
+        // arrange
+        $this->assertSame($wantBook, $createdBook);
+    }
+
+    public function testCreateBookThrowExceptionWhenAuthorNotExists()
+    {
+        $givenAuthorId = 10;
+        $givenTitle    = 'first PHP';
+
+        // arrange
+        $authorRepository = Mockery::mock(AuthorRepository::class);
+        $authorRepository
+            ->shouldReceive('getOrNullById')
+            ->with($givenAuthorId)
+            ->andReturn(null);
+
+        $bookRepository = Mockery::mock(BookRepository::class);
+        $bookRepository
+            ->shouldReceive('create')
+            ->never();
+
+        // expect
+        $this->expectException(AuthorNotFoundException::class);
+        $this->expectExceptionMessage('ID: 10のAuthorが存在しません');
+
+        // action
+        $usecase = new BookUsecase($authorRepository, $bookRepository);
+        $usecase->createBook($givenAuthorId, $givenTitle);
+    }
+}
